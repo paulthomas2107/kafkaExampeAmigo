00e8369c6136a1124c191820766da9e8c40d612c
Paul Thomas <paulthomas2107@gmail.com>
2022-10-28 11:10:15 +0100

    More...

--- src/main/java/com/pault/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/pault/kafkademo/KafkaDemoApplication.java
@@ -1,8 +1,11 @@
 package com.pault.kafkademo;
 
 import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 @Log
@@ -11,6 +14,16 @@ public class KafkaDemoApplication {
 	public static void main(String[] args) {
 		log.info("Starting......");
 		SpringApplication.run(KafkaDemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String,String> kafkaTemplate) {
+		return args -> {
+			for (int i = 0; i < 100; i++) {
+				kafkaTemplate.send("amigoscode", "Hello Paul ! :)...count is "+i);
+			}
+
+		};
 	}
 
 }
